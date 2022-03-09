cbfa0a1b351b10b987e4e836973c33f0ea57d4eb
LopezOne <antonio.lopez41@um.es>
2022-03-09 10:51:38 +0000

    DPI charm added

---
+++ juju-charms/dpi/src/charm.py
@@ -0,0 +1,80 @@
+#! /usr/bin/env python3
+
+import logging
+
+from ops.charm import CharmBase
+from ops.main import main
+from ops.model import ActiveStatus, MaintenanceStatus, BlockedStatus
+import subprocess
+import os
+
+logger = logging.getLogger(__name__)
+
+
+class SnortCharm(CharmBase):
+    """Class representing this Operator charm."""
+
+    def __init__(self, *args):
+        """Initialize charm and configure states and events to observe."""
+        super().__init__(*args)
+
+        self.framework.observe(self.on.config_changed, self.configure_pod)
+        self.framework.observe(self.on.add_rule_action, self._on_add_rule_action)
+        self.framework.observe(self.on.run_action, self._on_run_action)
+        self.framework.observe(self.on.touch_action, self._on_touch_action)
+
+    def _on_touch_action(self, event):
+        """Create an empty file receiving the path and filename as input"""
+        filename = event.params["filename"]
+        try:
+            subprocess.run(["touch", filename])
+            event.set_results({
+                "output": f"File {filename} created successfully"
+            })
+        except Exception as e:
+            event.fail(f"Touch action failed with the following exception: {e}")
+
+    def _on_run_action(self, event):
+        """Execute command receiving the command as input"""
+        cmd = event.params["cmd"]
+        try:
+            os.system(cmd)
+            event.set_results({
+                "output": f"Command: {cmd} executed successfully"
+            })
+        except Exception as e:
+            event.fail(f"Command: {cmd} failed with the following exception: {e}") 
+
+    def _on_add_rule_action(self, event):
+        """Add rule to Snort config"""
+        pass
+
+    def configure_pod(self, event):
+        if not self.unit.is_leader():
+            self.unit.status = ActiveStatus()
+            return
+        self.unit.status = MaintenanceStatus("Applying pod spec")
+        containers = [
+            {
+                "name": self.framework.model.app.name,
+                "image": "lopeez97/snort2:latest",
+                "ports": [
+                    {
+                        "name": "squid",
+                        "containerPort": 22,
+                        "protocol": "TCP",
+                    }
+                ],
+                "command": ["/bin/bash","-ce","tail -f /dev/null",],
+            }
+        ]
+
+        self.model.pod.set_spec({"version": 3, "containers": containers})
+
+        self.unit.status = ActiveStatus()
+        self.app.status = ActiveStatus()
+
+
+
+if __name__ == "__main__":
+    main(SnortCharm)
