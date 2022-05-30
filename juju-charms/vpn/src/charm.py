e6777406655871d46751c1fb201a049429bf350b
LopezOne <antonio.lopez41@um.es>
2022-05-30 13:30:22 +0000

    New actions added to VPN SC

--- juju-charms/vpn/src/charm.py
+++ juju-charms/vpn/src/charm.py
@@ -19,7 +19,10 @@ class VPNCharm(CharmBase):
 
         self.framework.observe(self.on.config_changed, self.configure_pod)
         self.framework.observe(self.on.run_action, self._on_run_action)
-        self.framework.observe(self.on.start_netflow_action, self._on_start_netflow_action)
+        self.framework.observe(self.on.init_vpnconfig_action, self._on_init_vpnconfig_action)
+        self.framework.observe(self.on.start_vpn_action, self._on_start_vpn_action)
+        self.framework.observe(self.on.create_clientcert_action, self._on_create_clientcert_action)
+
 
     def _on_run_action(self, event):
         """Execute command receiving the command as input"""
@@ -32,9 +35,45 @@ class VPNCharm(CharmBase):
         except Exception as e:
             event.fail(f"Command: {cmd} failed with the following exception: {e}")
 
-    def _on_start_netflow_action(self, event):
+    def _on_init_vpnconfig_action(self, event):
         """Initial configuration of OpenVPN"""
-        pass
+        domain = event.params["domain"]
+        genconfig = "ovpn_genconfig -u udp://" + domain
+        initpki = "ovpn_initpki nopass"
+        try:
+            res = os.system(genconfig)
+            res2 = os.system(initpki)
+            event.set_results({
+                "output": f"VPN configuration generated correctly"
+            })
+        except Exception as e:
+            event.fail(f"Command: {cmd} failed with the following exception: {e}")
+
+    def _on_start_vpn_action(self, event):
+        """Start OpenVPN execution"""
+        cmd = "ovpn_run"
+        try:
+            res = os.system(cmd)
+            event.set_results({
+                "output": f"VPN started successfully"
+            })
+        except Exception as e:
+            event.fail(f"Command: {cmd} failed with the following exception: {e}")
+
+    def _on_create_clientcert_action(self, event):
+        """Start OpenVPN execution"""
+        clientname = event.params["clientname"]
+        cmd = "easyrsa build-client-full " + clientname + " nopass"
+        cert = "ovpn_getclient " + clientname
+
+        try:
+            res = os.system(cmd)
+            res2 = os.system(cert)
+            event.set_results({
+                "output": f"Client cert created and retrieved successfully"
+            })
+        except Exception as e:
+            event.fail(f"Command: {cmd} failed with the following exception: {e}")
 
     def configure_pod(self, event):
         if not self.unit.is_leader():
