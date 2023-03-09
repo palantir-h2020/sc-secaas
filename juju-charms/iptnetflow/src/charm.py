70b6315695245daa4b85aa3b610eb13d5a9cb8ff
LopezOne <antonio.lopez41@um.es>
2023-03-09 16:16:13 +0000

    Fixed minor errors

--- juju-charms/iptnetflow/src/charm.py
+++ juju-charms/iptnetflow/src/charm.py
@@ -152,7 +152,7 @@ class IptnetflowCharm(CharmBase):
         containers = [
             {
                 "name": self.framework.model.app.name,
-                "image": "lopeez97/iptnetflow:1.0.3",
+                "image": "lopeez97/iptnetflow:1.0.4",
                 "ports": [
                     {
                         "name": "iptnetflow",
